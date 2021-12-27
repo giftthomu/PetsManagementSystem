cd15651d7fb73e0a9cabba8cd4eb362a8426171d
giftthomu <giftthomu@gmail.com>
2021-12-27 12:21:54 +0200

    Initial commit

--- app/src/main/java/com/petsmanagement/myapplication/DashboardAdmin.kt
+++ app/src/main/java/com/petsmanagement/myapplication/DashboardAdmin.kt
@@ -1,11 +1,18 @@
 package com.petsmanagement.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_dashboard_admin.*
 
 class DashboardAdmin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_admin)
+
+   registerVetCard.setOnClickListener {
+       val i = Intent(this, RegisterVetsActivity::class.java)
+       startActivity(i)
+   }
     }
 }
